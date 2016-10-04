330a56c2f315916caad33c11229daaeae8a712b0
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-04 15:19:24 +1300

    methods working and logging correctly

--- Consoletest.cs
+++ Consoletest.cs
@@ -8,9 +8,12 @@ namespace ConsoleApplication
             Console.WriteLine("testing console.writeline");
             int[] myArr = new int[10];
             for(int i = 0; i < myArr.Length; i++){
-                myArr[i] = i*2 + 1;
+                myArr[i] = Double(i) + 1;
                 Console.WriteLine(myArr[i]);
             }
+        }
+        public static int Double (int i) {
+            return i*2;
         }
     }
 }
