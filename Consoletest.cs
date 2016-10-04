63a76833b4676e6e0d837f8e28279dd946f009ee
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-04 15:08:57 +1300

    arrays working and logging correctly

--- Consoletest.cs
+++ Consoletest.cs
@@ -3,8 +3,12 @@ using System;
 namespace ConsoleApplication
 {
     public class ConsoleTest {
+       
         public static void Main (string[] args){
             Console.WriteLine("testing console.writeline");
+            int[] myArr = new int[3];
+            myArr[0] = 1;
+            Console.WriteLine("the first element in my array is: " + myArr[0]);
         }
     }
 }
